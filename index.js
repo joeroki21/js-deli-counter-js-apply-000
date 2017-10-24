e580ed4a58c5ecb71b2ca503f5da82b26e498758
Joseph Lawson <josephdlawson21@gmail.com>
2017-10-24 15:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(line){
   if (line.length === 0){
     return 'The line is currently empty.';
   } else {
-    for(var i = 0; i < line.length; i++){
+    for(var i = 0; i <= line.length; i++){
       result += (i+1) + '. ' + line.shift() + ', ';
   }
   }

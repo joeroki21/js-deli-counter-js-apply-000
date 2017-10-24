d3520b90af41d460358d7b9aaaec8545018b4868
Joseph Lawson <josephdlawson21@gmail.com>
2017-10-24 15:52:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function nowServing(katzDeli){
     return 'Currently serving ' + katzDeli.shift() + '.';
   }
 }
+
+function currentLine(line){
+  var result = 'The line is currently: ';
+  if (line.length === 0){
+    return 'The line is currently empty.';
+  } else {
+    for(var i = 0; i < line.length; i++){
+      result += (i+1) + '. ' + line.shift() + ', ';
+  }
+  }
+  return result
+}
+

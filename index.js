6a993c83d13399387f342e63c5f603ab247ec15a
Joseph Lawson <josephdlawson21@gmail.com>
2017-10-24 15:34:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 
 
-debugger;
+
 var katzDeli = []
 function takeANumber(Deli, name){
   katzDeli = Deli;
   katzDeli.push(name);
   return ('Welcome, ' + name + '. You are number ' + (katzDeli.indexOf(name)+1) + ' in line.')
+}
+
+function nowServing(katzDeli){
+  if(katzDeli.length === 0){
+    return 'There is nobody waiting to be served!';
+  } else {
+    return 'Currently serving ' + katzDeli.pop() + ' .';
+  }
 }
